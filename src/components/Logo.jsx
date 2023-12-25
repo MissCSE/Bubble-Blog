d4162e625c4099627b604ad11125a71c4d06b7e0
misscse <cse.nehasharma@gmail.com>
2023-12-25 16:19:23 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      Bubble
+      <image src="https://img.freepik.com/premium-vector/natural-realistic-bubble-illustration-vector-design_610945-5807.jpg" />
+      
+    </div>
+  )
+}
+
+export default Logo
